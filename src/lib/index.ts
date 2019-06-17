b52b916e694d708b8567550082ab9771274f6108
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-06-17 14:57:32 +0200

    Actually shim trimLeft

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,8 @@
 import 'react-app-polyfill/ie11';
-import 'string.prototype.trimleft';
+import trimLeft from 'string.prototype.trimleft';
+if (!String.prototype.trimLeft) {
+    trimLeft.shim();
+}
 
 export * from './date';
 export * from './date-annotator';
